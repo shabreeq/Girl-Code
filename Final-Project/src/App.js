c6a5ca8bc51ac29aa209aeb6992e292d987470c8
ShaBree Quinones <shabreeq@gmail.com>
2019-03-11 16:47:07 -0400

    starting over

--- Final-Project/src/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Navbar from './components/Navbar';
-
-const App = () => <MainPage />
-
-export default App;
--- keys.js
+++
@@ -1,7 +0,0 @@
-exports.youtube = {
-  id: process.env.youtube_API_Key
-};
-
-exports.meetup = {
-  id: process.env.meetup_API_Key
-};
