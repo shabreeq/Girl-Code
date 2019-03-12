e75923db2575d9b1fbbca013523955c610ec9ef4
ShaBree Quinones <shabreeq@gmail.com>
2019-03-11 23:36:26 -0400

    saving

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+var instance = M.Sidenav.getInstance(elem);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  instances = M.Sidenav.init(elems, options);
+});
+
+instance.open();
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  instances = M.Parallax.init(elems, options);
+});
+
+
+$(window).on("load", function(){
+  $.ready.then(function(){
+   $('.parallax').parallax();
+     $('#loading').fadeOut(800);
+  });
+})
--- public/js/materialize.js
+++ public/js/materialize.js
@@ -1,4 +1,9 @@
+var instance = M.Sidenav.getInstance(elems);
+
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.parallax');
-  var instances = M.Parallax.init(elems, options);
+  var elems = document.querySelectorAll('.sidenav');
+  instances = M.Sidenav.init(elems, onOpenStart);
 });
+
+
+instances.open();
