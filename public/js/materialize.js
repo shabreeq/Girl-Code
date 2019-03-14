3c20a8ab19615ef1352809a5204f53a4a1060560
ShaBree Quinones <shabreeq@gmail.com>
2019-03-14 18:31:23 -0400

    removing materialize

--- public/js/materialize.js
+++
@@ -1,9 +0,0 @@
-var instance = M.Sidenav.getInstance(elems);
-
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  instances = M.Sidenav.init(elems, onOpenStart);
-});
-
-
-instances.open();
